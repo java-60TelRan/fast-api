65b2e53dadf7bc777817e9c0eb690901e13d05dc
Yuragranovsky <yuragranovsky@gmail.com>
2025-12-08 22:08:25 +0200

    CW #40 FastAPI / Validation/ Logging

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 
 from models.item import Item
 from models.item_result import ItemResult
+from logger import logger
 ITEMS_DB: list[ItemResult] = []
 app = FastAPI()
+@app.middleware("http")
+async def logging_middleware(request: Request, call_next):
+    logger.info("method: %s, path: %s, port: %s", request.method, request.url.path, request.url.port)
+    resp: Response = await call_next(request)
+    logger.info("status code: %s", resp.status_code)
+    return resp
 @app.exception_handler(RequestValidationError)
 async def requestValidationHandler(req: Request, exc: RequestValidationError):
     return JSONResponse(
@@ -31,6 +38,8 @@ async def health():
     return {"status": "running"}
 @app.post("/items")
 async def create_item(item: Item):
+    logger.debug("product name: %s, price: %s, quantity: %s",
+                 item.productName, item.price, item.quantity)
     itemRes: ItemResult = ItemResult(
         productName=item.productName,
         price=item.price,
--- run_server.py
+++ run_server.py
@@ -4,5 +4,7 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8010,
-        reload=False
+        reload=True,
+        log_level="debug",
+        workers=3
     )
